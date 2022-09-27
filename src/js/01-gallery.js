b86ad504c2531c6a795806853e4dd4e55945c14c
Dmytro Tashpikov <neodim84@gmail.com>
2022-09-27 15:06:15 +0300

    1 and 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+const galleryCont = document.querySelector('.gallery');
+
+function createImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" width="800" height="600" alt="${description}"></a>`;
+    })
+    .join('');
+}
+
+galleryCont.insertAdjacentHTML('beforeend', createImg(galleryItems));
+
+galleryCont.addEventListener('click', clickOnImg);
+
+function clickOnImg(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+  });
+}
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelectorAll('.feedback-form label');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  console.log(`Email: ${email.value}, Message: ${message.value}`);
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+form.addEventListener('input', throttle(handleInput, 500));
+
+function handleInput(evt) {
+  const {
+    elems: { email, message },
+  } = evt.target.value;
+  localStorage.setItem('feedback-form-state', { email, message });
+  console.log({ email, message });
+}
+
+// function handleText() {
+//   const {
+//     elements: { email, message },
+//   } = localStorage.getItem('feedback-form-state');
+//   if (elements) {
+//     input.value = savedMessages;
+//   }
+// }
+
+// handleText();
