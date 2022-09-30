03cf6b2163aa34f5f3209ce1165ea0109eba1bfe
Dmytro Tashpikov <neodim84@gmail.com>
2022-09-30 09:22:55 +0300

    full

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,41 @@
+// 'use strict';
 import throttle from 'lodash.throttle';
+import { save, load, remove } from './storage';
 
 const form = document.querySelector('.feedback-form');
-const input = document.querySelectorAll('.feedback-form label');
+const LOCALE_STORAGE_KEY = 'feedback-form-state';
 
-form.addEventListener('submit', handleSubmit);
+initPage();
 
-function handleSubmit(evt) {
+const onFormInput = evt => {
+  const { name, value } = evt.target;
+  let saveData = load(LOCALE_STORAGE_KEY);
+  saveData = saveData ? saveData : {};
+  saveData[name] = value;
+  save(LOCALE_STORAGE_KEY, saveData);
+};
+const throttledOnFormInput = throttle(onFormInput, 500);
+
+form.addEventListener('submit', throttledOnFormInput);
+
+function initPage() {
+  const saveData = load(LOCALE_STORAGE_KEY);
+  if (!saveData) {
+    return;
+  }
+  Object.entries(saveData).forEach(([name, value]) => {
+    form.elements[name].value = value;
+  });
+}
+
+const handleSubmit = evt => {
   evt.preventDefault();
   const {
     elements: { email, message },
   } = evt.currentTarget;
-  console.log(`Email: ${email.value}, Message: ${message.value}`);
+  console.log({ email: email.value, message: message.value });
   evt.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-}
+  remove(LOCALE_STORAGE_KEY);
+};
 
-form.addEventListener('input', throttle(handleInput, 500));
-
-function handleInput(evt) {
-  const {
-    elems: { email, message },
-  } = evt.target.value;
-  localStorage.setItem('feedback-form-state', { email, message });
-  console.log({ email, message });
-}
-
-// function handleText() {
-//   const {
-//     elements: { email, message },
-//   } = localStorage.getItem('feedback-form-state');
-//   if (elements) {
-//     input.value = savedMessages;
-//   }
-// }
-
-// handleText();
+form.addEventListener('submit', handleSubmit);
